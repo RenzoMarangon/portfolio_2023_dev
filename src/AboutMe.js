5831e7e7afaed1aaa5c71e99c143605ee5a12f81
Renzo Marangon <renzomarangon@gmail.com>
2023-10-01 02:15:48 -0300

    v0.6 | about-me has improved

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,22 +7,24 @@ export const AboutMe = () => {
   return (
 
 
-    <div className='about-me flex flex-col items-center justify-center'>
-        <h2>Acerca de mí</h2>
+    <div className='about-me grid grid-cols-3 grid-rows-6  w-100 sm:w-5/6 xl:w-2/3 flex h-5/6'>
+        <div className='col-span-1 row-span-1'></div>
+        <h2 className='col-span-2 row-span-1 self-center font-bold text-lg'>Acerca de mí</h2>
 
-        <img src={`${ perfil }`} />
-        <div>
+        <img src={`${ perfil }`} className='rounded-2xl shadow-md w-100 w-10/12 col-span-1 justify-self-center'  />
+        <div className='col-span-2 flex flex-col items-left'>
             <p>lorem</p>
 
             <h4>Skills</h4>
-            <ul>
-                <li>Javascript</li>
-                <li>React</li>
-                <li>Node</li>
-                <li>Next.js</li>
-                <li>C++</li>
-                <li>Git</li>
-                <li>Tailwind</li>
+
+            <ul className='flex w-4/5  items-center flex-wrap'>
+                <li className='m-1 px-2 py-1 rounded-full bg-gray-100/25 shadow'>Javascript</li>
+                <li className='m-1 px-2 py-1 rounded-full bg-gray-100/25 shadow'>React</li>
+                <li className='m-1 px-2 py-1 rounded-full bg-gray-100/25 shadow'>Node</li>
+                <li className='m-1 px-2 py-1 rounded-full bg-gray-100/25 shadow'>Next.js</li>
+                <li className='m-1 px-2 py-1 rounded-full bg-gray-100/25 shadow'>C++</li>
+                <li className='m-1 px-2 py-1 rounded-full bg-gray-100/25 shadow'>Git</li>
+                <li className='m-1 px-2 py-1 rounded-full bg-gray-100/25 shadow'>Tailwind</li>
             </ul>
 
             <div>
@@ -36,7 +38,6 @@ export const AboutMe = () => {
                 </ul>
 
                 <button> Descargar cv</button>
-
             </div>
         </div>
     </div>
